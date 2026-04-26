d2d09bf06108a73f4aea6103ed3c29bb5fffa16d
Javlon <mohiiish.com@gmail.com>
2026-04-26 13:50:36 +0500

    daily1

--- next.config.js
+++ next.config.js
@@ -7,27 +7,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.daily.co",
-              "frame-src https://*.daily.co",
-              "connect-src 'self' wss://*.daily.co https://*.daily.co ws://localhost:* wss://localhost:* https://*.mongodb.net",
-              "media-src 'self' https://*.daily.co blob:",
-              "img-src 'self' data: https://*.daily.co",
-              "style-src 'self' 'unsafe-inline'",
-            ].join("; "),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
